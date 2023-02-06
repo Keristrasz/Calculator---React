8b7e2db1f9cbf4dd89888ca1f3e4a966595e9b43
Karel Ježík <117266468+Keristrasz@users.noreply.github.com>
2023-02-06 18:45:31 +0000

    added badge

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+  <React.StrictMode>
+    <App />
+    <a
+      id="badge"
+      target="_blank"
+      href="https://github.com/Keristrasz/Calculator---React"
+    >
+      <div>Code on GitHub</div>
+    </a>
+  </React.StrictMode>
 )
